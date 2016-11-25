fa8c1417e43769a104fce6b3cf07b627ccec5454
Alfonso, Hector I <hialfonso@mtf-nhac-1045-hia.local>
2016-11-25 14:40:41 -0500

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Alfonso, Hector I. on 11/25/16.
+//  Copyright © 2016 Alfonso, Hector I. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
